9a1cf745afa6f8b682208864f7e41781bd3d316b
Nicole <nicole.cai@clinicloud.com>
2016-05-21 18:53:38 +1000

    add configuration file

--- HistoryApp/LoginViewController.swift
+++ HistoryApp/LoginViewController.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2016 RMIT. All rights reserved.
 //
 import UIKit
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
     @IBOutlet var loginEmail: UITextField!
     
@@ -16,10 +17,32 @@ class LoginViewController: UIViewController {
     @IBOutlet var loginRegister: UIButton!
 
     @IBOutlet var loginAction: UIButton!
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
     
 // Lifecycle method for performing tasks after the view has loaded
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+    
+    // pressed the Sign In button
+    func signInWillDispatch(signIn: GIDSignIn!, error: NSError!) {
+        
+    }
+    
+    // Present a view that prompts the user to sign in with Google
+    func signIn(signIn: GIDSignIn!,
+                presentViewController viewController: UIViewController!) {
+        self.presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    // Dismiss the "Sign in with Google" view
+    func signIn(signIn: GIDSignIn!,
+                dismissViewController viewController: UIViewController!) {
+        self.dismissViewControllerAnimated(true, completion: {
+            
+        })
     }
 
 }
